2adb6434217b44b35f8df706e6b6d7196ab9f5f0
Marin Tony <72577254+Marin-Tony@users.noreply.github.com>
2021-04-11 17:38:02 +0530

    views.py of users

---
+++ users/views.py
@@ -0,0 +1,27 @@
+#from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+
+@login_required
+def register(request):
+	if request.user.username!='admin':
+		return redirect('not-authorised')
+	if request.method=='POST':
+		form=UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save() ###add user to database
+			messages.success(request, f'Registered successfully!')
+			return redirect('dashboard')
+	else:
+		form=UserCreationForm()
+	return render(request,'users/register.html', {'form' : form})
+
+
+
